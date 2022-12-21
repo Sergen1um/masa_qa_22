c339b9330096645178cd4952a1065e9ccc0313bc
Sergen1um <komseralc.isr@gmail.com>
2022-12-21 14:12:41 +0200

    HomeTask1

---
+++ java_project/src/Lesson_2/HomeTask/HomeTask.java
@@ -0,0 +1,55 @@
+package Lesson_2.HomeTask;
+
+public class HomeTask {
+    public static void main(String[] args) {
+        System.out.println("1.");
+        byte minbyte = Byte.MIN_VALUE, maxbyte = Byte.MAX_VALUE, randombyte = 100;
+        short minshort = Short.MIN_VALUE, maxshort = Short.MAX_VALUE, randomshort = 10000;
+        int minint = Integer.MIN_VALUE, maxint = Integer.MAX_VALUE, randomint = 1000000000, binint = 0b010101, hexint = 0x34ABCDEF, octaint = 0777;
+        long minlong = Long.MIN_VALUE, maxlong = Long.MAX_VALUE, randomlong = 1000000000000000000l;
+        System.out.println("minbyte = " + minbyte);
+        System.out.println("maxbyte = " + maxbyte);
+        System.out.println("randombyte = " + randombyte);
+        System.out.println("minshort = " + minshort);
+        System.out.println("maxshort = " + maxshort);
+        System.out.println("randomshort = " + randomshort);
+        System.out.println("minint = " + minint);
+        System.out.println("maxint = " + maxint);
+        System.out.println("randomint = " + randomint);
+        System.out.println("binint = " + binint);
+        System.out.println("haxint = " + hexint);
+        System.out.println("octaint = " + octaint);
+        System.out.println("minlong = " + minlong);
+        System.out.println("maxlong = " + maxlong);
+        System.out.println("randomlong = " + randomlong);
+        System.out.println();
+        System.out.println("2.");
+        float minfloat = Float.MIN_VALUE, maxfloat = Float.MAX_VALUE, randomfloat = 1.0f;
+        double mindouble = Double.MIN_VALUE, maxdouble = Double.MAX_VALUE, randomdouble = 1.0;
+        boolean falseboolean = Boolean.FALSE, trueboolean = Boolean.TRUE;
+        System.out.println("minfloat = " + minfloat);
+        System.out.println("maxfloat = " + maxfloat);
+        System.out.println("randomfloat = " + randomfloat);
+        System.out.println("mindouble = " + mindouble);
+        System.out.println("maxdouble = " + maxdouble);
+        System.out.println("randomdouble = " + randomdouble);
+        System.out.println("falseboolean = " + falseboolean);
+        System.out.println("trueboolean = " + trueboolean);
+        System.out.println();
+        System.out.println("3.");
+        char char1 = 'A';
+        char char2 = '\u0041';
+        char char3 = '\101';
+        char char4 = 65;
+        char char5 = 0x00000041;
+        char char6 = 0x0041;
+        char char7 = 0x41;
+        System.out.println("char1 = " + char1);
+        System.out.println("char2 = " + char2);
+        System.out.println("char3 = " + char3);
+        System.out.println("char4 = " + char4);
+        System.out.println("char5 = " + char5);
+        System.out.println("char6 = " + char6);
+        System.out.println("char7 = " + char7);
+    }
+}
