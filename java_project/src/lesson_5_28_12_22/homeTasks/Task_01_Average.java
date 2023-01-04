e350cb995b5fc3f332477470cdf72fc0c42d4958
Sergen1um <komseralc.isr@gmail.com>
2023-01-04 18:50:44 +0200

    Update Task_01_Average.java

--- java_project/src/lesson_5_28_12_22/homeTasks/Task_01_Average.java
+++ java_project/src/lesson_5_28_12_22/homeTasks/Task_01_Average.java
@@ -3,12 +3,12 @@ package lesson_5_28_12_22.homeTasks;
 public class Task_01_Average {
     public static void main(String[] args) {
 
-        double lastNumber = 100;
+        int lastNumber = 100;
         int summary = 0;
 
         for (int i = 0; i < (lastNumber + 1); i++) {
             summary = summary + i;
         }
-        System.out.println(summary / (lastNumber + 1));
+        System.out.println((double)summary / (lastNumber + 1));
     }
 }
